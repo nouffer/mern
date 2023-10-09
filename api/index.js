28fa4839ff88254d6206dcf0295f094a1f9e06f8
Mohamed Nouffer <nouffer@gmail.com>
2023-10-09 21:45:20 +0530

    Created test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose
@@ -16,3 +17,9 @@ const app = express();
 app.listen(3000, () => {
   console.log("server runs on port 3000");
 });
+
+app.get("/test", (req, res) => {
+  res.send("Hello from Node");
+});
+
+app.use("/api/user", userRouter);
