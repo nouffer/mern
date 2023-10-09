bcf300bf8a53a335935b9c23ee8a8a4a164a1139
Mohamed Nouffer <nouffer@gmail.com>
2023-10-09 22:38:11 +0530

    created signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -14,6 +15,7 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log("server runs on port 3000");
 });
@@ -23,3 +25,4 @@ app.get("/test", (req, res) => {
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
