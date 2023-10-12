657bf2143272138dc21012f4d5f2820d4267c250
Mohamed Nouffer <nouffer@gmail.com>
2023-10-12 16:08:48 +0530

    done profile update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 mongoose
@@ -16,6 +18,9 @@ mongoose
 
 const app = express();
 app.use(express.json());
+
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log("server runs on port 3000");
 });
