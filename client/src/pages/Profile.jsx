1fb22b18630987e10f9c667e04af81de76535fab
Mohamed Nouffer <nouffer@gmail.com>
2023-10-14 19:49:35 +0530

    create listing UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -19,6 +19,7 @@ import {
   signoutUserFailure,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 function Profile() {
   const { currentUser, error, loading } = useSelector((state) => state.user);
@@ -191,6 +192,12 @@ function Profile() {
         >
           {loading ? "Loading" : "Update"}
         </button>
+        <Link
+          className=" bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to="/create-listing"
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
