fb013d6c065dd05152dc2e3d94878755d42c8120
Mohamed Nouffer <nouffer@gmail.com>
2023-10-16 18:38:08 +0530

    done update listing api

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from "../controllers/listing.controller.js";
 import { varifyToken } from "../utils/varifyUser.js";
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.post("/create", varifyToken, createListing);
 router.delete("/delete/:id", varifyToken, deleteListing);
+router.post("/update/:id", varifyToken, updateListing);
 
 export default router;
